1874c2e87aa9a95fe20957da84f818896dcd6357
Paul M Furley <paul@paulfurley.com>
2017-07-18 10:36:26 +0100

    Deploy to `build` not `gh-pages`

--- config.rb
+++ config.rb
@@ -55,7 +55,7 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  deploy.branch   = 'gh-pages'
+  deploy.branch   = 'build'
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
