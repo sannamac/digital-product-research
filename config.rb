d65e877bed6db4631b4f6bddf88b60b2b0f70454
Paul M Furley <paul@paulfurley.com>
2017-07-18 10:36:49 +0100

    Cleanup commented config

--- config.rb
+++ config.rb
@@ -53,9 +53,5 @@ end
 
 activate :deploy do |deploy|
   deploy.deploy_method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   deploy.branch   = 'build'
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
